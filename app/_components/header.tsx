4ef7c89379ca47a0a8a98fd18abfaa4922792312
YuriMotaTech <yuri_motamelo@hotmail.com>
2025-11-16 20:55:36 -0300

    feat: add header and image components

---
+++ app/_components/header.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { HomeIcon, MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between px-5 py-6 bg-white">
+      <Image src="/logo.svg" alt="Barbershop Lobby" width={100} height={26.09} />
+      <Button variant="outline" size="icon">
+        <MenuIcon />
+      </Button>
+    </header>
+  );
+};
+
+export default Header;
